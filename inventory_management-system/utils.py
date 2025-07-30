c61ca19793e1a675588ce7d598d65c17ea97ed21
ydejene <y.dejene@alustudent.com>
2025-07-30 10:15:52 +0300

    Update utils.py

--- inventory_management-system/utils.py
+++ inventory_management-system/utils.py
@@ -1,13 +1,10 @@
 import os
-
-
 def clear_screen():
     """Clear the terminal screen"""
     if os.name == 'nt':  
         os.system('cls')
     else: 
         os.system('clear')
-
 def pause():
     """Wait for user to press Enter"""
     input("\nPress Enter to return to main menu...")
